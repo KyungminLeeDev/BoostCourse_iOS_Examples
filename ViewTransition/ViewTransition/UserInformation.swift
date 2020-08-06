52a965646738cca2fc01e78efbec2d9507029afc
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-08-06 21:11:45 +0900

    UserInformation.swift 파일 생성하여 클래스 작성

---
+++ ViewTransition/ViewTransition/UserInformation.swift
@@ -0,0 +1,20 @@
+//
+//  UserInformation.swift
+//  ViewTransition
+//
+//  Created by Kyungmin Lee on 2020/08/06.
+//  Copyright © 2020 Kyungmin Lee. All rights reserved.
+//
+
+import Foundation
+
+class UserInformation {
+    // 싱글턴
+    // shared같이 자주 사용하는 이름을 사용하면 해킹에 취약할 수 있으므로
+    // 더 공부후 익숙해지면 다른 이름을 사용해보자
+    static let shared: UserInformation = UserInformation()
+    
+    var name: String?
+    var age: String?
+    
+}
