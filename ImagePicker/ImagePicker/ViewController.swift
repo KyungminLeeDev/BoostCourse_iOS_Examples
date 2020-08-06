7fe7d390c6b9bf5037590cba38e6187d86ea10f4
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-08-06 16:11:10 +0900

    ViewController에 이미지피커 델리게이트 추가
    스토리보드에 이미지뷰, 버튼 추가 후 코드와 연결

--- ImagePicker/ImagePicker/ViewController.swift
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    lazy var imagePicker: UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self // ViewController가 picker의 델리게이트 역할을 할 것이다 라고 명시
+        return picker
+    }()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func touchUpSelectImageButton(_ sender: UIButton) {
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
