085b4e6d6f0b6deb34c31caa97d8311ccac76f70
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-08-10 17:45:23 +0900

    ViewController에 tapView() 액션 메서드 작성후 스토리보드에서 Tap Gesture Reconizer와 연결

--- TabGesture/TabGesture/ViewController.swift
+++ TabGesture/TabGesture/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
