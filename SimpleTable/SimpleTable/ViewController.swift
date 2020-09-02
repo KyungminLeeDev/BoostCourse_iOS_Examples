9351da0ff8645e254034b165c758af68651da379
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-09-02 23:24:42 +0900

    테이블 뷰의 섹션과 타이틀 사용

--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
     @IBOutlet weak var tableview: UITableView!
     let cellIdentifier: String = "cell"
@@ -20,8 +22,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 뷰 컨트롤러의 인스턴스가 테이블뷰의 델리게이트와 데이터소스 역할을 해줄 것이다
+        tableview.delegate = self
+        tableview.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
 
+    // 섹션에 해당하는 row의 갯수가 몇개인지 알려달라
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+            case 0:
+                return korean.count
+            case 1:
+                return english.count
+            default:
+                return 0
+        }
 
+    }
+    
+    // row에 해당하는 cell을 달라
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
+        
+        cell.textLabel?.text = text
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "한글" : "영어"
+    }
 }
 
