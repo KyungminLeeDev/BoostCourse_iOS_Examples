5834572e26500d1fd58ad1919ef26d4a3874093c
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-09-02 20:55:32 +0900

    2. 기상정보 애플리케이션 예제 프로젝트 추가

---
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Kyungmin Lee on 2020/09/02.
+//  Copyright © 2020 Kyungmin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
