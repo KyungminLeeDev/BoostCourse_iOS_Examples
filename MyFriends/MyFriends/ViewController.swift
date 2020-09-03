01fd6413ee340b5ebd0a12a49ba8a26d8e4c9f6f
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-09-03 18:03:20 +0900

    JSON 다루기 예제 프로젝트 추가

---
+++ MyFriends/MyFriends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFriends
+//
+//  Created by Kyungmin Lee on 2020/09/03.
+//  Copyright © 2020 Kyungmin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
