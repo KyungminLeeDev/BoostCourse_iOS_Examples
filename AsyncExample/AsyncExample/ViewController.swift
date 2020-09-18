45c50d0d71e1adb164c7faf40366eb5e17a70577
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-09-18 17:18:29 +0900

    AsyncExample 프로젝트 추가
    - SceneDelegate 제거

---
+++ AsyncExample/AsyncExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AsyncExample
+//
+//  Created by Kyungmin Lee on 2020/09/18.
+//  Copyright © 2020 Kyungmin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
