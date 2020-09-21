67cceab3df27764ecf7843bcc228f13c776a101a
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-09-21 20:09:06 +0900

    FriendsCollection 프로젝트 생성

---
+++ FriendsCollection/FriendsCollection/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  FriendsCollection
+//
+//  Created by Kyungmin Lee on 2020/09/21.
+//  Copyright © 2020 Kyungmin Lee. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
