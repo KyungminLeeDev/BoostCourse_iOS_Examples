ecd497c14bbdf912b040ef0b50122e8e6107a0fe
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-22 21:21:56 +0900

    Feat #3 - 서버에 데이터 요청하고 받아오기

--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -18,7 +18,36 @@ class ViewController: UIViewController, UITableViewDataSource {
         // Do any additional setup after loading the view.
     }
     
-    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        guard let url: URL = URL(string: "https://randomuser.me/api/?results=20&inc=name,email,picture") else {
+            return
+        }
+        
+        let session: URLSession = URLSession(configuration: .default)
+        let dataTask: URLSessionDataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let apiResponse: APIResponse = try JSONDecoder().decode(APIResponse.self, from: data)
+                self.friends = apiResponse.results
+                self.tableView.reloadData()
+            } catch(let error) {
+                print(error.localizedDescription)
+            }
+        }
+        
+        dataTask.resume()
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
