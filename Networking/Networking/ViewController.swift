a6c8d557d97618efa550779eb5f3ec610cc0b344
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-22 21:05:02 +0900

    Feat #3 - 테이블뷰 델리게이트 메서드 구현

--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -18,13 +18,28 @@ class ViewController: UIViewController, UITableViewDataSource {
         // Do any additional setup after loading the view.
     }
     
+    
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        let friend: Friend = self.friends[indexPath.row]
         
-        return UITableViewCell()
+        cell.textLabel?.text = friend.name.full
+        cell.detailTextLabel?.text = friend.email
+        
+        guard let imageURL: URL = URL(string: friend.picture.thumbnail) else {
+            return cell
+        }
+        guard let imageData: Data = try? Data(contentsOf: imageURL) else {
+            return cell
+        }
+        cell.imageView?.image = UIImage(data: imageData)
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return friends.count
     }
     
     
