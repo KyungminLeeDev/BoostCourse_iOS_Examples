5f3e295c7395c6f9de5a39898a84c18b0ceb93a8
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-22 20:53:05 +0900

    Feat #3 - 테이블뷰 추가, 연동

--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let cellIdentifier: String = "friendCell"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
 }
 
